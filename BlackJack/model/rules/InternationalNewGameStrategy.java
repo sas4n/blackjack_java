0937a9c5bcad2a9dc6cef8b5eaf69e1189763544
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 21:39:39 +0200

    Update InternationalNewGameStrategy.java

--- BlackJack/model/rules/InternationalNewGameStrategy.java
+++ BlackJack/model/rules/InternationalNewGameStrategy.java
@@ -5,22 +5,27 @@ import BlackJack.model.Dealer;
 import BlackJack.model.Player;
 import BlackJack.model.Card;  
 
-class InternationalNewGameStrategy implements INewGameStrategy {
+class InternationalNewGameStrategy extends Factory implements INewGameStrategy {
 
   public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
-    Card c;
-  
-    c = a_deck.GetCard();
-    c.Show(true);
-    a_player.DealCard(c);
-  
-    c = a_deck.GetCard();
-    c.Show(true);
-    a_dealer.DealCard(c);
-  
-    c = a_deck.GetCard();
-    c.Show(true);
-    a_player.DealCard(c);
+    /** Rona added */
+    getCard(a_deck, a_player, true);
+    getCard(a_deck, a_dealer, true);
+    getCard(a_deck, a_player, true);
+
+    //    Card c;
+//
+//    c = a_deck.GetCard();
+//    c.Show(true);
+//    a_player.DealCard(c);
+//
+//    c = a_deck.GetCard();
+//    c.Show(true);
+//    a_dealer.DealCard(c);
+//
+//    c = a_deck.GetCard();
+//    c.Show(true);
+//    a_player.DealCard(c);
   
     return true;
   }
