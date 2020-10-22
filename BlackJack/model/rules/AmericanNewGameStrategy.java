13c808a2e033cb8e9d1a23f149c349a6ab8d9214
Xingrong Zong <xz222bb@student.lnu.se>
2020-10-22 21:39:30 +0200

    Update AmericanNewGameStrategy.java

--- BlackJack/model/rules/AmericanNewGameStrategy.java
+++ BlackJack/model/rules/AmericanNewGameStrategy.java
@@ -5,26 +5,32 @@ import BlackJack.model.Dealer;
 import BlackJack.model.Player;
 import BlackJack.model.Card;  
 
-class AmericanNewGameStrategy implements INewGameStrategy {
+class AmericanNewGameStrategy extends Factory implements INewGameStrategy {
 
   public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
-    Card c;
+    /** Rona added */
+    getCard(a_deck, a_player, true);
+    getCard(a_deck, a_dealer, true);
+    getCard(a_deck, a_player, true);
+    getCard(a_deck, a_dealer, false);
 
-    c = a_deck.GetCard();
-    c.Show(true);
-    a_player.DealCard(c);
-
-    c = a_deck.GetCard();
-    c.Show(true);
-    a_dealer.DealCard(c);
-
-    c = a_deck.GetCard();
-    c.Show(true);
-    a_player.DealCard(c);
-
-    c = a_deck.GetCard();
-    c.Show(false);
-    a_dealer.DealCard(c);
+//    Card c;
+//
+//    c = a_deck.GetCard();
+//    c.Show(true);
+//    a_player.DealCard(c);
+//
+//    c = a_deck.GetCard();
+//    c.Show(true);
+//    a_dealer.DealCard(c);
+//
+//    c = a_deck.GetCard();
+//    c.Show(true);
+//    a_player.DealCard(c);
+//
+//    c = a_deck.GetCard();
+//    c.Show(false);
+//    a_dealer.DealCard(c);
 
     return true;
   }
