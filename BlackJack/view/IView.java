f1e741b63ce3bcb4c4ff109b8ebc68bbcd07545f
xiaosheshe97 <xiaosheshe97@gmail.com>
2020-10-22 23:15:45 +0200

    Update IView.java

--- BlackJack/view/IView.java
+++ BlackJack/view/IView.java
@@ -3,9 +3,15 @@ package BlackJack.view;
 public interface IView
 {
   void DisplayWelcomeMessage();
-  int GetInput();
+  //I put into IBasicView
+  //int GetInput();
   void DisplayCard(BlackJack.model.Card a_card);
   void DisplayPlayerHand(Iterable<BlackJack.model.Card> a_hand, int a_score);
   void DisplayDealerHand(Iterable<BlackJack.model.Card> a_hand, int a_score);
   void DisplayGameOver(boolean a_dealerIsWinner);
+    void collectEvents();
+    boolean wantsToStartNewGame();
+    boolean wantsToHit();
+    boolean wantsToStand();
+    boolean wantsToQuit();
 }
