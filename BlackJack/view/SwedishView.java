c15d14a0f1d1b1aa5c25c688c9a613ed94ce98fb
Rahim Pormah <rp222hu@student.lnu.se>
2020-10-22 23:38:42 +0200

    Update SwedishView.java

--- BlackJack/view/SwedishView.java
+++ BlackJack/view/SwedishView.java
@@ -5,7 +5,7 @@ public class SwedishView extends BasicView implements IView
         public void DisplayWelcomeMessage()
         {
          
-            for(int i = 0; i < 50; i++) {System.out.print("\n");};
+            for(int i = 0; i < 5; i++) {System.out.print("\n");};
 
             System.out.println("Hej Black Jack Världen");
             System.out.println("----------------------");
