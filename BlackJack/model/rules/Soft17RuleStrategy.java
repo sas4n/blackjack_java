8ec559964e8200b6ba31984d04019963d7eb100b
Rahim Pormah <rp222hu@student.lnu.se>
2020-10-23 19:39:05 +0200

    Update Soft17RuleStrategy.java

--- BlackJack/model/rules/Soft17RuleStrategy.java
+++ BlackJack/model/rules/Soft17RuleStrategy.java
@@ -1,10 +1,29 @@
 package BlackJack.model.rules;
 
+import BlackJack.model.Card;
+import BlackJack.model.Dealer;
 import BlackJack.model.Player;
 
 public class Soft17RuleStrategy implements IHitStrategy{
+
     @Override
     public boolean DoHit(Player a_dealer) {
+       if(isSoft17(a_dealer)) {
+           if(a_dealer.CalcScore() == 17) {
+               return true;
+           }
+       }
+
+           return a_dealer.CalcScore() <17;
+    }
+
+    private boolean isSoft17(Player a_dealer){
+
+            for(Card c : a_dealer.GetHand()){
+                if(c.GetValue() == Card.Value.Ace)
+                    return true;
+            }
+
         return false;
     }
 }
