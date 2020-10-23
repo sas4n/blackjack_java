46a18b1cd7ce6ac91b9fe1063d1065b224406d0d
Rahim Pormah <rp222hu@student.lnu.se>
2020-10-23 18:24:58 +0200

    Update Factory.java

--- BlackJack/model/rules/Factory.java
+++ BlackJack/model/rules/Factory.java
@@ -1,13 +1,40 @@
 package BlackJack.model.rules;
 
 import BlackJack.model.Card;
+import BlackJack.model.Dealer;
 import BlackJack.model.Deck;
 import BlackJack.model.Player;
 
-public abstract class Factory {
+public abstract class Factory implements INewGameStrategy {
     public void getCard(Deck deck, Player role, boolean show) {
         Card card = deck.GetCard();
         card.Show(show);
         role.DealCard(card);
+    }
+    public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
+        /** Rona added */
+        getCard(a_deck, a_player, true);
+        getCard(a_deck, a_dealer, true);
+        getCard(a_deck, a_player, true);
+        //we have to fix it in order to have no duplication
+
+
+        //    Card c;
+//
+//    c = a_deck.GetCard();
+//    c.Show(true);
+//    a_player.DealCard(c);
+//
+//    c = a_deck.GetCard();
+//    c.Show(true);
+//    a_dealer.DealCard(c);
+//
+//    c = a_deck.GetCard();
+//    c.Show(true);
+//    a_player.DealCard(c);
+
+
+
+        return true;
     }
 }
