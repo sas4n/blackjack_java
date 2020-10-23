0aa9851e3028b811cf327beaae768ea2760e2653
xiaosheshe97 <xiaosheshe97@gmail.com>
2020-10-23 18:05:41 +0200

    Update Player.java

--- BlackJack/model/Player.java
+++ BlackJack/model/Player.java
@@ -4,83 +4,77 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedList;
 
-public class Player {
+public class Player implements ICardPublisher {
 
-  private List<Card> m_hand;
-  protected final int g_maxScore = 21;
-  //private ArrayList<IGameObserver> subscribers;
+    private List<Card> m_hand;
+    protected final int g_maxScore = 21;
+    private ArrayList<ICardObserver> subscribers;
 
-  public Player()
-  {
-    m_hand = new LinkedList<Card>();
-    //subscribers = new ArrayList<>();
-  }
-//  public void addSubscriber(IGameObserver subscriber){
-//      //subscribers.add(subscriber);
-//  }
-  public void DealCard(Card a_addToHand)
-  {
-    m_hand.add(a_addToHand);
-//    for (IGameObserver obs: subscribers){
-//        obs.DisplayedCard(a_addToHand);
-//    }
-//    try{
-//        Thread.sleep(2000);
-//    }catch (Exception e){
-//
-//    }
-  }
-  
-  public Iterable<Card> GetHand()
-  {
-    return m_hand;
-  }
-  
-  public void ClearHand()
-  {
-    m_hand.clear();
-  }
-  
-  public void ShowHand()
-  {
-    for(Card c : GetHand())
-    {
-      c.Show(true);
+    public Player() {
+        m_hand = new LinkedList<Card>();
+        subscribers = new ArrayList<>();
     }
-  }
-  
-  public int CalcScore()
-  {
-    // the number of scores is dependant on the number of scorable values
-    // as it seems there is no way to do this check at compile time in java ?!
-    // cardScores[13] = {...};
-    int cardScores[] = {
-        2, 3, 4, 5, 6, 7, 8, 9, 10, 10 ,10 ,10, 11
-    };
-    assert (cardScores.length == Card.Value.Count.ordinal()) : "Card Scores array size does not match number of card values";
-  
-    
-    int score = 0;
 
-    for(Card c : GetHand()) {
-        if (c.GetValue() != Card.Value.Hidden)
-        {
-            score += cardScores[c.GetValue().ordinal()];
+    @Override
+    public void Subscribe(ICardObserver subscriber) {
+        if (subscribers.contains(subscriber) == false) {
+            subscribers.add(subscriber);
         }
     }
 
-    if (score > g_maxScore)
-    {
-        for(Card c : GetHand())
-        {
-            if (c.GetValue() == Card.Value.Ace && score > g_maxScore)
-            {
-                score -= 10;
+    @Override
+    public void Notify(Card card) {
+        for (ICardObserver obs : subscribers) {
+            obs.updateNewCard(card);
+        }
+    }
+
+    public void DealCard(Card a_addToHand) {
+        Notify(a_addToHand);
+        m_hand.add(a_addToHand);
+    }
+
+    public Iterable<Card> GetHand() {
+        return m_hand;
+    }
+
+    public void ClearHand() {
+        m_hand.clear();
+    }
+
+    public void ShowHand() {
+        for (Card c : GetHand()) {
+            c.Show(true);
+        }
+    }
+
+    public int CalcScore() {
+        // the number of scores is dependant on the number of scorable values
+        // as it seems there is no way to do this check at compile time in java ?!
+        // cardScores[13] = {...};
+        int cardScores[] = {
+                2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11
+        };
+        assert (cardScores.length == Card.Value.Count.ordinal()) : "Card Scores array size does not match number of card values";
+
+
+        int score = 0;
+
+        for (Card c : GetHand()) {
+            if (c.GetValue() != Card.Value.Hidden) {
+                score += cardScores[c.GetValue().ordinal()];
             }
         }
-    }
 
-    return score;
-  }
+        if (score > g_maxScore) {
+            for (Card c : GetHand()) {
+                if (c.GetValue() == Card.Value.Ace && score > g_maxScore) {
+                    score -= 10;
+                }
+            }
+        }
+
+        return score;
+    }
 
 }
